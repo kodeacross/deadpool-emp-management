8395afc7432fb766908d467c063fab8f17d2b81f
Seiji Ralph Villafranca <seijiv13@outlook.com>
2024-10-19 09:23:11 +0200

    auth0 setup for login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, inject, OnInit } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,17 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'deadpool-employee-management';
+  auth = inject(AuthService);
+
+  
+  public ngOnInit() {
+    this.auth.isAuthenticated$.subscribe((authenticated) => {
+      if(!authenticated) {
+        this.auth.loginWithRedirect();
+      }
+    })
+  }
+ 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,14 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAuth0 } from '@auth0/auth0-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAuth0({
+    domain: 'dev-oci2vqoeei4eau6g.jp.auth0.com',
+    clientId: '7p5pev8bDDKYYLqhC7ORCSciaYWptuBf',
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })]
 };
